5e9f2e1cbcc92d46002f920cd45c74f988fceed0
Sharan <sharansr@student.ubc.ca>
2024-01-29 14:44:33 -0800

    Changed data retrieval script

--- retrieve-data.sh
+++ retrieve-data.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 JGI_SESSION_TOKEN=""
+FILENAME="output.zip"
 
 # Download genome fasta file
 curl --cookie jgi_session=$JGI_SESSION_TOKEN \
-     --output download.20240122.160059.zip \
-     -d "{\"ids\":{\"Phytozome-200\":[\"52b9c7b9166e730e43a34f15\"]}}" \
+     --output $FILENAME \
+     -d "{\"ids\":{\"Phytozome-200\":[\"53112aae49607a1be005599c\",\"53112ab149607a1be00559a3\"]}}" \
      -H "Content-Type: application/json" https://files-download.jgi.doe.gov/filedownload/
 
 # Unzip and move files to proper directory
-unzip download.20240122.160059.zip
-unzip download.20240122.160059/**/*.zip
-mv download.20240122.160059/Phytozome/PhytozomeV9/Lusitatissimum/assembly/Lusitatissimum_200.fa \
-   data/
-
+unzip $FILENAME
