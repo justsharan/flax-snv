5b9d43df92e4a95ca86977cc2ffd7caafad5b1c8
Sharan S <sharansr@pm.me>
2024-03-30 23:08:21 -0700

    Filtering criteria should use logical OR

--- filters.sh
+++ filters.sh
@@ -4,7 +4,7 @@
 bcftools filter -Ou -g 5:indel,other $1 | bcftools view -Ob -M 2 -v snps > remove_indels.bcf
 
 # Hard filters
-bcftools filter -Oz -e "INFO/DP>2.5*AVG(INFO/DP) & QUAL<30 & MQ<30 & SP>3" remove_indels.bcf > filtered.vcf.gz
+bcftools filter -Oz -e "INFO/DP>2.5*AVG(INFO/DP) | QUAL<30 | MQ<30 | SP>3" remove_indels.bcf > filtered.vcf.gz
 
 # SnpEff
 java -jar $snpEff/snpEff.jar flax filtered.vcf.gz -csvStats stats.csv | gzip > annotated.vcf.gz
