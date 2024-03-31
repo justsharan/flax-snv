426ecdae6ebacf703a7914765eb410a350371b25
Sharan S <sharansr@pm.me>
2024-03-30 23:02:13 -0700

    Added filters

---
+++ filters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove INDELs, bases around INDELs, multiallelic sites
+bcftools filter -Ou -g 5:indel,other $1 | bcftools view -Ob -M 2 -v snps > remove_indels.bcf
+
+# Hard filters
+bcftools filter -Oz -e "INFO/DP>2.5*AVG(INFO/DP) & QUAL<30 & MQ<30 & SP>3" remove_indels.bcf > filtered.vcf.gz
+
+# SnpEff
+java -jar $snpEff/snpEff.jar flax filtered.vcf.gz -csvStats stats.csv | gzip > annotated.vcf.gz
+
+# Filter for HIGH or MODERATE variants
+java -jar $snpEff/SnpSift.jar filter "(ANN[*].IMPACT has 'HIGH') | (ANN[*].IMPACT has 'MODERATE')" annotated.vcf.gz | gzip > high_moderatre.vcf.gz
