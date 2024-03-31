78567e66e0b3480ead9ec1e050902fec482d16b8
Sharan S <sharansr@pm.me>
2024-03-30 23:20:45 -0700

    Produce relevant R plots

--- vcf_analysis.R
+++ vcf_analysis.R
@@ -1,19 +1,46 @@
-install.packages('vcfR')
+library(tidyverse)
+library(CMplot)
+library(patchwork)
 
-library(vcfR)
+vcf.cols <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", 1:61)
 
-vcf_file <- 'out/vcf/SRR1610817.vcf.gz'
-dna_file <- 'Phytozome/PhytozomeV10/Lusitatissimum/assembly/Lusitatissimum_200_BGIv1.0.fa'
-gff_file <- 'Phytozome/PhytozomeV10/Lusitatissimum/annotation/Lusitatissimum_200_v1.0.gene_exons.gff3'
+# VCF file after hard filtering
+hard.filtered.variants <- read_tsv("filtered.vcf.gz", comment = "#", col_names = vcf.cols)
+# VCF file for high/moderate variants
+high.moderate.variants <- read_tsv("high_moderate.vcf.gz", comment = "#", col_names = vcf.cols)
 
-vcf <- read.vcfR(vcf_file, verbose=FALSE)
-dna <- ape::read.dna(dna_file, format='fasta')
-gff <- read.table(gff_file, sep='\t', quote='')
+## PLOTTING
 
-unique(gff[,1])
+hard.filtered.snps <- hard.filtered.variants |>
+    mutate(Type = paste(REF, ">", ALT, sep = "")) |>
+    count(Type) |>
+    mutate(Prop = n / sum(n)) |>
+    ggplot(aes(x = reorder(Type, -Prop), y = Prop)) +
+        geom_bar(stat = "identity") +
+        labs(x = "SNP Type", y = "Relative Frequency") +
+        ggtitle("All Filtered Variants") +
+        scale_y_continuous(labels = scales::percent) +
+        theme_classic()
 
-chrom <- create.chromR(name='Supercontig', vcf=vcf, seq=dna, ann=gff)
+high.moderate.snps <- high.moderate.variants |>
+    mutate(Type = paste(REF, ">", ALT, sep = "")) |>
+    count(Type) |>
+    mutate(Prop = n / sum(n)) |>
+    ggplot(aes(x = reorder(Type, -Prop), y = Prop)) +
+        geom_bar(stat = "identity") +
+        labs(x = "SNP Type", y = "Relative Frequency") +
+        ggtitle("High and Moderate Impact Variants") +
+        scale_y_continuous(labels = scales::percent) +
+        theme_classic()
 
-plot(chrom)
+# Render bar plots
+hard.filtered.snps + high.moderate.snps
+ggsave("snp_type.jpg", hard.filtered.snps + high.moderate.snps, width = 9, height = 6)
 
-chromoqc(chrom, dp.alpha=20)
+# Generate SNP Density Plot
+hard.filtered.variants |>
+    select(CHROM, POS) |>
+    mutate(SNP = paste("SNP", row_number(), sep = ""), CHROM = substring(CHROM, 3)) |>
+    relocate(SNP, CHROM, POS) |>
+    CMplot(plot.type = "d", chr.den.col = c("darkgreen", "yellow", "red"),
+        file.name = "density_plot", file = "jpg")
